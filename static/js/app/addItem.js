d41b9f8094061240291ddd11d131db95bc5b2a58
Bruce <444048170@qq.com>
2017-08-24 12:07:16 +0800

    to fix some bugs

--- static/js/app/addItem.js
+++ static/js/app/addItem.js
@@ -172,7 +172,9 @@ function getReviewMode() {
 			         '" data-regulation="'+ regulation +'" >' + title + '</li>';
 			reviewModel[id] = title;
 			$("#Popover_1 .mui-scroll").append(li);
-			$("#Popover_1 .mui-scroll-wrapper").css("height", ($("#Popover_1 li").length * 25 + 10) + "px");
+			var height = $("#Popover_1 li").length * 25 + 10);
+			if (height>200) height = 200;
+			$("#Popover_1 .mui-scroll-wrapper").css("height", (height + "px");
 		}
 	});	
 }
@@ -192,7 +194,9 @@ function getTaskType() {
 			planType[id] = title;
 			$("#Popover_2 .mui-scroll").append(li);
 		}
-		$("#Popover_2 .mui-scroll-wrapper").css("height", ($("#Popover_2 li").length * 25 + 10) + "px");
+		var height = $("#Popover_2 li").length * 25 + 10);
+		if (height>200) height = 200;
+		$("#Popover_2 .mui-scroll-wrapper").css("height", (height + "px");
 	});	
 }
 
